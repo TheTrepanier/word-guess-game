7badd4c9a80dc0dec3a7bdbc498d0ab975e614a4
TheTrepanier <thetrepanier1@Michaels-MacBook-Pro-3.local>
2019-03-02 13:33:14 -0700

    ran JS through a linter to check for errors, added audio file supprise

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,24 @@
 // Create a request variable and assign a new XMLHttpRequest object to it.
 var request = new XMLHttpRequest();
 
-var randomNumber = Math.floor(Math.random() * 807);
-var wins = 0;
-var losses = 0;
+var randomNumber = Math.floor((Math.random() * 807) + 1);
+var itHappens = Math.floor((Math.random() * 23) + 1);
 var guessesRemaining = 5;
 var correctGuesses = 0;
 var pokemonName = "";
-var nameBlankSpaces = "";
+var pokemonID = 0;
 var pokemonNameArray = [];
 var playerHasPicked = [];
 
 function newGame() {
     location.reload();
+}
+
+function doIt() {
+    var audio = document.getElementById("audio");
+    audio.play();
+    document.getElementById("root").innerHTML = "<h1> The wild " + pokemonName + " got away!";
+    document.getElementById("refresh").innerHTML = "<button type='button' class='btn btn-danger btn-lg' onclick='newGame()'>Play Again?</button>";
 }
 
 // Open a new connection, using the GET request on the URL endpoint
@@ -25,6 +31,7 @@ request.onload = function() {
     if (request.status >= 200 && request.status < 400) {
         pokemonName = data.name;
         pokemonNameArray = data.name.split("");
+        pokemonID = data.id;
         var blankPokemonName = "";
 
         for (let index = 0; index < pokemonNameArray.length; index++) {
@@ -37,51 +44,60 @@ request.onload = function() {
     } else {
         console.log('error');
     }
-}
+};
 
-document.onkeyup = function(event) {
-    var playerPick = event.key;
-    var selectedIds = [];
-    
-    // determine if there are multiple occurances of the same charecter
-    if (pokemonNameArray.includes(playerPick)) {
-        for (let index = 0; index < pokemonNameArray.length; index++) {
-            var idToSelect = pokemonNameArray.indexOf(playerPick);
-            
-            if (idToSelect >= 0) {
-                selectedIds.push(idToSelect);
-                delete pokemonNameArray[idToSelect];
+window.onload = function() {
+    document.onkeyup = function(event) {
+        var inp = String.fromCharCode(event.keyCode);
+        var selectedIds = [];
+        if (/[a-zA-Z0-9-_ ]/.test(inp)) {
+            var playerPick = event.key;
+
+            // determine if there are multiple occurances of the same charecter
+            if (pokemonNameArray.includes(playerPick)) {
+                for (let index = 0; index < pokemonNameArray.length; index++) {
+                    var idToSelect = pokemonNameArray.indexOf(playerPick);
+                
+                    if (idToSelect >= 0) {
+                        selectedIds.push(idToSelect);
+                        delete pokemonNameArray[idToSelect];
+                    }
+                }
+                playerHasPicked.push(playerPick);
+            } else if (playerHasPicked.indexOf(playerPick) >= 0) {
+                console.log("you already picked that letter!");
+            } else {
+                if (guessesRemaining > 0) {
+                    guessesRemaining--;
+                    document.getElementById("remaining").innerHTML = guessesRemaining;
+                } 
+                if (guessesRemaining === 0) {
+                    document.getElementById("root").innerHTML = "<h1> The wild " + pokemonName + " got away!";
+                    document.getElementById("refresh").innerHTML = "<button type='button' class='btn btn-danger btn-lg' onclick='newGame()'>Play Again?</button>";
+                }
+                playerHasPicked.push(playerPick);
+            }
+            // update the displayed name of the pokemon
+            for (let index = 0; index < selectedIds.length; index++) {
+                document.getElementById(selectedIds[index]).innerHTML = playerPick;
+                correctGuesses++;
+
+                if (correctGuesses === pokemonName.length) {
+                    document.getElementById("title").innerHTML = "Gotcha!";
+                    document.getElementById("subtitle").innerHTML = pokemonName;
+                    document.getElementById("subtitle").classList.add("cap");
+                    document.getElementById("pokemon-sprite").innerHTML = "<img src='assets/images/pokeball.png' width='190'>";
+                    document.getElementById("pokemon-name-blank-spaces").style.display = "none";
+                    document.getElementById("caption").innerHTML = "was captured!";
+                    document.getElementById("refresh").innerHTML = "<button type='button' class='btn btn-danger btn-lg' onclick='newGame()'>Play Again!</button>";
+                }
             }
         }
-        playerHasPicked.push(playerPick);
-    } else if (playerHasPicked.indexOf(playerPick) >= 0) {
-        console.log("you already picked that letter!");
-    } else {
-        guessesRemaining--
-
-        if (guessesRemaining === 0) {
-            document.getElementById("root").innerHTML = "<h1> The wild " + pokemonName + " got away!";
-            document.getElementById("refresh").innerHTML = "<button type='button' class='btn btn-danger btn-lg' onclick='newGame()'>Play Again?</button>"
-        }
+    };
+    if (itHappens === 3 && pokemonID != 25) {
+        document.getElementById("theThing").innerHTML = "<button type='button' class='btn btn-danger btn-lg' onclick='doIt()'>IT\'S PIKACHU!</button>";
     }
-    // update the displayed name of the pokemon
-    for (let index = 0; index < selectedIds.length; index++) {
-        document.getElementById(selectedIds[index]).innerHTML = playerPick;
-        correctGuesses++
-
-        if (correctGuesses === pokemonName.length) {
-            document.getElementById("title").innerHTML = "Gotcha!";
-            document.getElementById("subtitle").innerHTML = pokemonName;
-            document.getElementById("subtitle").classList.add("cap")
-            document.getElementById("pokemon-sprite").innerHTML = "<img src='assets/images/pokeball.png' width='190'>";
-            document.getElementById("pokemon-name-blank-spaces").style.display = "none";
-            document.getElementById("caption").innerHTML = "was captured!"
-            document.getElementById("refresh").innerHTML = "<button type='button' class='btn btn-danger btn-lg' onclick='newGame()'>Play Again!</button>"
-        }
-    }
-    
-}
-
+};
 
 
 // send request
